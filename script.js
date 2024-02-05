72a71a21bcc15459c716a04f3413276f8bf508df
Paramvir SIngh <148181589+param442@users.noreply.github.com>
2024-02-05 16:38:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let tl=gsap.timeline();
+tl.from("nav",{
+    opacity:0,
+    delay:0.5,
+    duration:1,
+});
+tl.from("nav div h1",{
+    y:-100,
+    stagger:0.5,
+    duration:0.75,
+});
+tl.from(".section #subsection1 h1",{
+    x:-100,
+   stagger:0.5,
+    duration:0.75,
+    opacity:0,
+});
+tl.from(".section .img img",{
+    scale:0.5,
+    opacity:0,
+    duration:0.5,
+    
+});
+tl.from(".section2 .card",{
+    opacity:0,
+    y:100,
+    duration:1,
+    stagger:0.5,
+    scrollTrigger:{
+    trigger:".section2 .card",
+    scroller:"body",
+    markers:true,
+    start:"top 80%",
+    end:"top 60%",
+    scrub: 2,
+    }
+})
